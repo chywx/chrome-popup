d696367faf3ab6ab7f7ab92389579d8ac69f3628
陈海洋 <1559843332@qq.com>
2019-12-30 18:51:31 +0800

    popup与background通信 dahai

--- js/background.js
+++ js/background.js
@@ -25,16 +25,37 @@ console.log(d.toLocaleString());*/
 
 
 // 监听content_js的请求，并响应chyok
-chrome.extension.onRequest.addListener(
-    function (request, sender, sendResponse) {
-        console.log(sender.tab ?
-            "from a content script:" + sender.tab.url :
-            "from the extension");
-        if (request.greeting == "hello" && mp == 1)
-            sendResponse({farewell: "world"});
-        else
-            sendResponse({farewell: "goodbye"}); // snub them.
-    });
+// chrome.extension.onRequest.addListener(
+//     function (request, sender, sendResponse) {
+//         console.log(sender.tab ?
+//             "from a content script:" + sender.tab.url :
+//             "from the extension");
+//         if (request.greeting == "hello" && mp == 1)
+//             sendResponse({farewell: "world"});
+//         else
+//             sendResponse({farewell: "goodbye"}); // snub them.
+//     });
+
+// 监听content传递过来的数据
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (mp == 1) {
+        sendResponse('我已收到你的消息：' + JSON.stringify({"mp": "1,开始请求，保存session状态"}));//做出回应
+    }
+    sendResponse('我已收到你的消息：' + JSON.stringify(request));//做出回应
+});
+
+// 向content发送数据dahai
+// setInterval(function () {
+//     chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+//         chrome.tabs.sendMessage(tabs[0].id, {message: "calculate", greeting: "您好"}, function (response) {
+//             if (typeof response != 'undefined') {
+//                 alert(JSON.stringify(response));
+//             } else {
+//                 alert("response为空=>" + response);
+//             }
+//         });//end  sendMessage
+//     }); //end query
+// })
 
 
 //接收消息
--- js/contentScript.js
+++ js/contentScript.js
@@ -6,13 +6,37 @@
         // bac.postMessage({joke: "Knock knock"});
 
         // 发送请求chyok
-        setInterval(function () {
-                chrome.extension.sendRequest({greeting: "hello"}, function (response) {
-                    // 打印相应
-                    console.log(response.farewell);
+        // setInterval(function () {
+        //         chrome.extension.sendRequest({greeting: "hello"}, function (response) {
+        //             // 打印相应
+        //             console.log(response.farewell);
+        //
+        //         });
+        // },5000)
 
-                });
-        },5000)
+        // 向background发送数据
+        let iii = 0;
+        setInterval(function () {
+            chrome.runtime.sendMessage(
+                {greeting: '你好，我是content-script呀，我主动发消息给后台！' + ++iii},
+                function (response) {
+                    console.log('收到来自后台的回复：' + response);
+                }
+            );
+        }, 5000);
+
+
+        // 监听background传过来的数据dahai
+        // chrome.runtime.onMessage.addListener(
+        //     function (request, sender, sendResponse) {
+        //         console.log(sender.tab ? "来自内容脚本：" + sender.tab.url : "来自扩展程序");
+        //         console.log(JSON.stringify(request), request.greeting);
+        //         if (request.greeting == "您好") {
+        //             console.log("进行响应");
+        //             sendResponse({farewell: "再见"});
+        //         }
+        //     });
+
 
         // let aaa = 1;
         // //接收消息
@@ -31,12 +55,6 @@
         //         }
         //     })
         // })
-        /*chrome.extension.onConnect.addListener(function(cab) {
-            console.log("AAA")
-            cab.onMessage.addListener(function(msg) {
-                console.log(msg.greeting);
-                console.log("BBB")
-            })
-        })*/
+
     });
 })();
